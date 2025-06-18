2511691f6d94d94ee5e06166fd94728765a267a6
aidausmanova <aida.ct127@gmail.com>
2025-06-18 12:55:28 +0200

    Update: remove unnecessary files

--- backend/scripts/run_llm_assess.sh
+++ backend/scripts/run_llm_assess.sh
@@ -2,7 +2,7 @@
 #SBATCH --gpus-per-node=1
 #SBATCH --constraint=48GB
 #SBATCH --job-name=gri
-#SBATCH -o /storage/usmanova/reportGRI/backend/logs/run_eval_%j.out # STDOUT
+#SBATCH -o /reportGRI/backend/logs/run_eval_%j.out # STDOUT
 
 export CUDA_VISIBLE_DEVICES=0,1
 export VLLM_WORKER_MULTIPROC_METHOD=spawn
